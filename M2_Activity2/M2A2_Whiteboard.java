0df6f4dfc0233c44eee4f3bc61509bf0b201eef2
Jannella Rose T. De Guzman <jrtdeguzman@headoffice.corp.bpi.com.ph>
2025-12-10 12:04:31 +0800

    M2 A2 Posted

---
+++ M2_Activity2/M2A2_Whiteboard.java
@@ -0,0 +1,16 @@
+package M2_Activities;
+
+public class M2A2_Whiteboard {
+
+	
+	int addNumbers(int addend1, int addend2) {
+		
+		int sum = addend1 + addend2;
+		
+		return sum;
+		
+		
+	}
+	
+	
+}
---
+++ M2_Activity2/M2_Activity2.java
@@ -0,0 +1,20 @@
+package M2_Activities;
+
+//import java.util.Scanner;
+
+public class M2_Activity2 {
+
+	public static void main(String[] args) {
+		
+	
+		M2A2_Whiteboard wb = new M2A2_Whiteboard();
+		
+		int sum = wb.addNumbers (5,3);
+	
+        System.out.println("The total is: " + String.valueOf(sum));  
+		
+
+		
+	}
+
+}
